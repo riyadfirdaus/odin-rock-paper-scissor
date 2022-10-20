e37b639ce1faad5ee3275be98fb74dee9a43c16f
Riyad Firdaus <riyadf27@gmail.com>
2022-10-21 00:07:24 +0700

    Add UI and rework logic

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+let computerSelection, playerSelection;
+let scoreHuman = 0, scoreComputer = 0;
+let resultText;
+
+function convertToEmoji(text){
+    if(text == 'rock') return '✊';
+    if(text == 'paper') return '✋';
+    if(text == 'scissor') return '✌️';
+    return;
+}
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3)+1;
     switch(choice){
@@ -12,44 +22,85 @@ function getComputerChoice(){
             break;
     }
 }
+function computeResult(result){
+    if(result == 'win'){
+        scoreHuman++;
+    }
+    else if(result == 'lose'){
+        scoreComputer++;
+    }
+    updateCard();
+    if(scoreComputer == 5)
+        gameOver('Oh noo, you lose!');
+    if(scoreHuman == 5)
+        gameOver('Yeay, you win!');
+
+    return;
+}
+
+function gameOver(text){
+    const mainContent = document.getElementById('main');
+    console.log(mainContent);
+    //Remove Emoji Button
+    while (mainContent.firstChild) {
+        mainContent.removeChild(mainContent.lastChild);
+    }
+    //Append Text Result and Play Again Button
+    const gameResult = document.createElement('h3');
+    gameResult.innerText = text;
+    mainContent.appendChild(gameResult);
+
+    const button = document.createElement('button');
+    button.classList.add('btn');
+    button.innerText = 'Play Again';
+    button.addEventListener('click', () =>{
+        location.reload();
+    });
+    mainContent.appendChild(button);
+    
+}
+
+function updateCard(emoji){
+    const cardHuman = document.querySelector('#human');
+    const cardComputer = document.querySelector('#computer');
+    cardHuman.querySelector('#score').innerText = scoreHuman;
+    cardHuman.querySelector('.emoji').innerText = convertToEmoji(playerSelection);
+    cardComputer.querySelector('#score').innerText = scoreComputer;
+    cardComputer.querySelector('.emoji').innerText = convertToEmoji(computerSelection);
+
+}
 
 function playRound(playerSelection, computerSelection) {
-    let lose = "You lose! " + playerSelection +" lose to " +computerSelection;
-    let win = "You win! " + playerSelection +" beats " +computerSelection;
-    let draw = "Huftt It's a draw!";
     switch(playerSelection){
         case computerSelection:
-            return draw;
+            computeResult('draw');
             break;
         case "rock":
-            if(computerSelection == "scissor") 
-                return win;
-            else return lose;
+            if(computerSelection == "scissor") computeResult('win');
+            else computeResult('lose');
             break;
         case "paper":
-            if(computerSelection == "rock") return win;
-            else return lose;
+            if(computerSelection == "rock") computeResult('win');
+            else computeResult('lose');
             break;
         case "scissor":
-            if(computerSelection == "rock") return lose;
-            else return win;
+            if(computerSelection == "paper") computeResult('win');
+            else computeResult('lose');
             break;
         default:
             return "Wrong Input";
             break;
     }
-    return text;
 }
 
-function game(){
-    for(i=0; i<5;i++){
-        const computerSelection = getComputerChoice();
-    
-        let playerSelection = prompt("Input your choice");
-        playerSelection = playerSelection.toLowerCase();
-    
-        console.log(playRound(playerSelection, computerSelection));
-    }
+function selectChoice(e){
+    computerSelection = getComputerChoice();
+    playerSelection = this.id.toLowerCase();
+    playRound(playerSelection, computerSelection);
 }
 
-game();
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button =>{
+    button.addEventListener('click', selectChoice);
+});
+
